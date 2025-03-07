b42b8c7886b5295c9c6d564c45b65d4152223245
TheBaytur <bayturcodding@gmail.com>
2025-03-07 11:34:38 +0800

    Initial Commit

---
+++ SimpleRecipeApp/SimpleRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRecipeAppApp.swift
+//  SimpleRecipeApp
+//
+//  Created by Admin on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleRecipeAppTests/SimpleRecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRecipeAppTests.swift
+//  SimpleRecipeAppTests
+//
+//  Created by Admin on 3/7/25.
+//
+
+import Testing
+@testable import SimpleRecipeApp
+
+struct SimpleRecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
